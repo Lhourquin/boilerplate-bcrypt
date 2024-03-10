b88cfaa0ac81225772eb89c6ccad12b6270d1da5
lhourquin <lhourquin@gmail.com>
2024-03-10 17:32:39 +0100

    learn : bcrypt hash and compare, async and sync method

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcrypt')
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
@@ -10,43 +11,24 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash)=> {
+    if(err){
+        console.log(err)
+    }else {
+        bcrypt.compare(myPlaintextPassword, hash, (err, res)=> {
+            console.log(res)
+        })
+        console.log(hash)
+    }
+})
 //END_ASYNC
 
 //START_SYNC
-
-
-
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash)
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result)
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const PORT = process.env.PORT || 3000;
